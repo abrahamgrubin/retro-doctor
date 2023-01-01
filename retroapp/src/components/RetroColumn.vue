2db95d9273388418caba38c5dbddbd93f23ac2a8
Abraham Rubin <abrahamgrubin@gmail.com>
2023-01-01 17:48:01 +0000

    added nestable

--- retroapp/src/components/RetroColumn.vue
+++ retroapp/src/components/RetroColumn.vue
@@ -1,43 +1,45 @@
 <template>
     <ion-col class="ion-text-center">
-              <ion-card>
-                <ion-card-header>
-                  <ion-card-title>{{ this.retrodata.title }}</ion-card-title>
-                  <ion-card-subtitle>{{ this.retrodata.subtitle }}</ion-card-subtitle>
-                </ion-card-header>
-                <template #item="{ element, index }">
-                  <div class="list-group-item">{{ element.name }} {{ index }}</div>
-                </template>
-                <ion-card-content>
-                  <ion-list>
-                    <draggable
-                      class="list-group"
-                      :list="list1"
-                      group="people"
-                      @change="log"
-                      itemKey="name"
-                    >
-                    <template #item="{ element, index }">
-                      <ion-item class="list-group-item">
-                          <ion-thumbnail slot="start">
-                            <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/thumbnail.svg" />
-                          </ion-thumbnail>
-                          <ion-note><p>{{ element.name }} {{ index }}</p></ion-note>
-                      </ion-item>
-                    </template>
-                    </draggable>
-                  </ion-list>
-                </ion-card-content>
-              </ion-card>
-              </ion-col>
+      <ion-title><Notes /></ion-title>
+      
+    </ion-col>
 </template>
+              <!--<ion-card>-->
+              <!--  <ion-card-header>-->
+              <!--    <ion-card-title>{{ this.retrodata.title }}</ion-card-title>-->
+              <!--    <ion-card-subtitle>{{ this.retrodata.subtitle }}</ion-card-subtitle>-->
+              <!--  </ion-card-header>-->
+              <!--  <template #item="{ element, index }">-->
+              <!--    <div class="list-group-item">{{ element.name }} {{ index }}</div>-->
+              <!--  </template>-->
+              <!--  <ion-card-content>-->
+              <!--    <ion-list>-->
+              <!--      <draggable-->
+              <!--        class="list-group"-->
+              <!--        :list="list1"-->
+              <!--        group="people"-->
+              <!--        @change="log"-->
+              <!--        itemKey="name"-->
+              <!--      >-->
+              <!--      <template #item="{ element, index }">-->
+              <!--        <ion-item class="list-group-item">-->
+              <!--            <ion-thumbnail slot="start">-->
+              <!--              <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/thumbnail.svg" />-->
+              <!--            </ion-thumbnail>-->
+              <!--            <ion-note><p>{{ element.name }} {{ index }}</p></ion-note>-->
+              <!--        </ion-item>-->
+              <!--      </template>-->
+              <!--      </draggable>-->
+              <!--    </ion-list>-->
+              <!--  </ion-card-content>-->
+              <!--</ion-card>-->
 <script>
   import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonFab, IonFabButton, IonIcon, IonCol, IonGrid, IonRow, IonList,  IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle,  IonReorder, IonReorderGroup, IonMenu, IonMenuButton   } from '@ionic/vue';
   import { Authenticator,useAuthenticator } from '@aws-amplify/ui-vue';
   import { defineComponent, defineProps } from 'vue';
   import { add } from 'ionicons/icons';
   import { useRouter } from 'vue-router';
-  
+  import Notes from './Notes.vue';
   import draggable from 'vuedraggable';
   defineProps({
       retrodata: Object
@@ -68,6 +70,7 @@ export default defineComponent({
       IonCardTitle, 
       IonMenu, 
       IonMenuButton,
+      Notes
     },
     setup() {
       return {
@@ -95,6 +98,7 @@ export default defineComponent({
     },
     getTemplate() {
         console.log(this.$props.retrodata);
+        console.log(Notes)
     }
   }
   });
